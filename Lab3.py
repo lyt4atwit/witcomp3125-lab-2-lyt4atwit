0417acd184dba7a6ef7b314d85c3d78532ba29aa
Tony Ly <54997152+lyt4atwit@users.noreply.github.com>
2021-10-25 13:55:11 -0400

    Initial Commit

---
+++ Lab3.py
@@ -0,0 +1,64 @@
+# task: 1-player game to AI player game
+# create 3 walls, paded, ball, OOP, AI
+# import paddle and ball
+from paddle import Paddle
+from ball import Ball
+import pygame
+import random
+import math
+
+def main():
+    pygame.init()
+
+    pygame.display.set_caption("Lab3")
+    WIDTH = 800
+    HEIGHT = 480
+    BORDER = 20
+    VELOCITY = 5
+    FPS = 120
+ 
+    Screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.update()
+
+    # colors
+    fgColor = pygame.Color("cyan")
+    bgColor = pygame.Color("black")
+    
+    # WALLS
+    # rectangle params: Rect((left, top), (width, length))
+    # top wall
+    pygame.draw.rect(Screen, fgColor, pygame.Rect((0, 0), (WIDTH, BORDER)))
+    # left wall
+    pygame.draw.rect(Screen, fgColor, pygame.Rect((0, 0), (BORDER, HEIGHT)))
+    # bottom wall
+    pygame.draw.rect(Screen, fgColor, pygame.Rect((0, HEIGHT-BORDER), (WIDTH, BORDER)))
+    # ball
+    x0 = WIDTH - Ball.RADIUS
+    y0 = HEIGHT // 2
+    vx0 = -VELOCITY
+    vy0 = int(random.uniform(-VELOCITY, VELOCITY))
+
+    b0 = Ball(x0, y0, vx0, vy0, Screen, fgColor, bgColor)
+    b0.show(fgColor)
+
+    pygame.display.update()
+    # define a variable to control the main loop
+    running = True
+    clock = pygame.time.Clock()
+        
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+        pygame.display.update()
+        clock.tick(FPS)
+        # ball
+        b0.update(HEIGHT, BORDER, vy0)
+
+if __name__=="__main__":
+    # call the main function
+    main()
---
+++ ball.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Ball:
+    # pass
+    # class variables
+    RADIUS = 10
+
+    def __init__(self, x, y, vx, vy, screen, fgColor, bgColor):
+        # instance variables
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.screen = screen
+        self.fgColor = fgColor
+        self.bgColor = bgColor
+
+    def show(self, color):
+        pygame.draw.circle(self.screen, color, (self.x, self.y), self.RADIUS)
+
+    def update(self, height, border, vy0):
+        # math sanitizing 
+        if vy0 == 0:
+            vy0 = 1
+        if vy0 == -4:
+            vy0 = 4
+        # ball hits wall
+        if self.x == border + self.RADIUS: # 30
+            self.vx = -self.vx
+        if self.y == border + self.RADIUS + (border + self.RADIUS)%vy0 or self.y == height - border - self.RADIUS - (border + self.RADIUS)%vy0: # 30, 450
+            self.vy = -self.vy
+
+        # delete p0
+        self.show(self.bgColor)
+        
+        # pf = p0 + v
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+        self.show(self.fgColor)
